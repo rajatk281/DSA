c4e75e26f5a2fe5a19444d27423b35de79d0b572
NNobittaa <rajat28012006@gmail.com>
2026-02-15 09:52:07 +0530

    Printing max sum

---
+++ Array/Max_subarray_sum_Brute_force_approach.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include <climits>
+
+using namespace std; 
+
+void MaxSubSum(int *arr, int n){
+
+    int maxSum = INT_MIN;
+    for (int i=0; i<n; i++){
+        for (int j = i; j<n; j++){
+            int counter = 0;
+            for (int k = i; k<=j; k++){
+                // cout<<arr[k];
+                counter+=arr[k];
+            }
+            maxSum = max(maxSum, counter);
+            cout<<counter;
+            cout<<", ";
+        }
+        cout<<"\n";
+    }
+    cout<<maxSum;
+}
+
+int main(){
+    int arr[6] = {2, -3, 6, -5, 4, 2};
+    MaxSubSum(arr, 6);
+}
