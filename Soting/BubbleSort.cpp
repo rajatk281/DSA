4202714f315c1cb83016facc9a4fd1d72b8894a4
NNobittaa <rajat28012006@gmail.com>
2026-03-18 23:37:17 +0530

    modification

--- Soting/BubbleSort.cpp
+++ Soting/BubbleSort.cpp
@@ -1,24 +1,43 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-void bubbleSort(int arr[], int n){
-    for (int i = 0; i<n-1; i++){
-        for (int j = 0; j<n-i-1; j++){
-            if(arr[j]>arr[j+1]){
-                swap(arr[j], arr[j+1]);
+void bubbleSort(int arr[], int n)
+{
+    bool isSwap = false;
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                swap(arr[j], arr[j + 1]);
+                isSwap = true;
             }
+        }
+        if (!isSwap)
+        {
+            return;
         }
     }
 }
 
-int display(int arr[],int n){
-    for (int i = 0; i<n; i++){
-        cout<<arr[i]<<" ";
+int display(int arr[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
     }
 }
 
-int main(){
-    int arr[5] = {1, 5, 7, 2, 3,};
+int main()
+{
+    int arr[5] = {
+        1,
+        5,
+        7,
+        2,
+        3,
+    };
     bubbleSort(arr, 5);
     display(arr, 5);
 }
