950ccde79270685449d8eea2a5857825907c3e6d
NNobittaa <rajat28012006@gmail.com>
2026-03-19 13:19:45 +0530

    spiral matrix

---
+++ 2D Array/SpiralMatrix.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+using namespace std; 
+
+void spiralMatrix(int arr[][4], int n, int m){
+    int srow = 0;
+    int scol = 0;
+    int erow = n-1;
+    int ecol = m-1;
+
+    while(srow <= erow && scol <= ecol){
+        // top
+        for (int i=scol; i<=ecol; i++ ){
+            cout<<arr[srow][i] << " ";
+        }
+        // right
+        for (int i=srow+1; i<=erow; i++){
+            cout<<arr[i][ecol] << " ";
+        }
+
+        // bottom
+        for (int i=ecol-1; i>=scol; i--){
+            cout<<arr[erow][i] << " ";
+        }
+
+        // left 
+        for (int i = erow-1; i>srow; i-- ){
+            cout<<arr[i][scol] << " ";
+        }
+        srow++; 
+        scol++; 
+        erow--; 
+        ecol--; 
+}
+}
+
+int main(){
+    int mat[4][4] = {
+        {1, 2, 3, 4},
+        {5, 6, 7, 8},
+        {9, 10, 11, 12},
+        {13, 14, 15, 16}
+    };
+    spiralMatrix(mat, 4, 4);
+
+    return 1; 
+}
--- 2D Array/intro.cpp
+++ 2D Array/intro.cpp
@@ -4,6 +4,5 @@ using namespace std;
 int main(){
     int student[3][2] = {{80, 90}, {90, 100}, {90, 80}};
     return 1;
-    
 
 }
