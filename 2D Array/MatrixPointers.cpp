d2431b884261e674ccb7778cff83afd672aaf60b
NNobittaa <rajat28012006@gmail.com>
2026-03-29 05:16:52 +0530

    Matrix pointers

---
+++ 2D Array/MatrixPointers.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std; 
+
+
+
+int main(){
+    int mat[3][3] = {{1, 2, 3},
+{4, 5, 6},
+{7, 8, 9}};
+
+    cout << mat[0][0]<< endl ; 
+    cout << (mat + 1)[0][0] << endl ;
+
+    return 1; 
+}
