1e85c1df564602b8ec1d4eab6934921c05666332
NNobittaa <rajat28012006@gmail.com>
2026-03-28 10:02:32 +0530

    Optimized diagonal sum

--- 2D Array/DiagonalSum.cpp
+++ 2D Array/DiagonalSum.cpp
@@ -1,15 +1,28 @@
 #include<iostream>
 using namespace std; 
 
+// int diagonalSum(int arr[][3], int n){
+//     int sum = 0;
+//     for (int i = 0; i<n; i++){
+//         for (int j = 0; j<n; j++){
+//             if (i == j ){
+//                 sum += arr[i][j];
+//             } else if ( j == n-i-1) {
+//                 sum += arr[i][j]; 
+//             }
+//         }
+//     }
+//     return sum; 
+// }
+
+// Optimized diagonal sum 
+
 int diagonalSum(int arr[][3], int n){
     int sum = 0;
     for (int i = 0; i<n; i++){
-        for (int j = 0; j<n; j++){
-            if (i == j ){
-                sum += arr[i][j];
-            } else if ( j == n-i-1) {
-                sum += arr[i][j]; 
-            }
+        sum += arr[i][i];
+        if ( i != n-i-1){
+            sum += arr[i][n-i-1];
         }
     }
     return sum; 
