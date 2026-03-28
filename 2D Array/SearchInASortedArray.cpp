61be82de56934d69502317429f09f14f1804dd73
NNobittaa <rajat28012006@gmail.com>
2026-03-29 03:03:25 +0530

    Optimized Sorted array search using stepcase search

--- 2D Array/SearchInASortedArray.cpp
+++ 2D Array/SearchInASortedArray.cpp
@@ -1,16 +1,37 @@
 #include<iostream>
 using namespace std; 
 
-pair<int, int> searchInASortedArray(int arr[][4], int n, int key){
-    for (int i = 0; i < n; i++){
-        for(int j = 0; j < n; j++){
-            if(arr[i][j] == key){
-                return {i, j};
-            }
+// Brute force approach 
+// pair<int, int> searchInASortedArray(int arr[][4], int n, int key){
+//     for (int i = 0; i < n; i++){
+//         for(int j = 0; j < n; j++){
+//             if(arr[i][j] == key){
+//                 return {i, j};
+//             }
+//         }
+//     }
+//     return {-1, -1};
+// }
+
+bool optimizedSearch(int arr[][4], int row, int col, int key){
+    int i = 0 ;
+    int j = col-1; 
+    while (i < row && j >= 0){
+        if ( arr[i][j] == key) {
+            cout<< "(" << i <<", "<<j<<")"; 
+            return true;
+        }
+        else if ( arr[i][j] > key){
+            j--;
+        }
+        else{
+            i++; 
         }
     }
-    return {-1, -1};
-}
+    return false; 
+} 
+
+
 int main (){
     int arr[4][4] = {
         {1, 2, 3, 4},
@@ -19,8 +40,10 @@ int main (){
         {13, 14, 15, 16}
     };
 
-    pair<int, int> result = searchInASortedArray(arr, 4, 10);
+    // pair<int, int> result = searchInASortedArray(arr, 4, 10);
 
-    cout << "Row: " << result.first 
-         << ", Col: " << result.second;
+    optimizedSearch(arr, 4, 4, 7);
+
+    // cout << "Row: " << result.first 
+    //      << ", Col: " << result.second;
 }
