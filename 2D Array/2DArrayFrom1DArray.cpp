f38c73c40c5a8452c969b6421525f51add29d05b
NNobittaa <rajat28012006@gmail.com>
2026-04-01 07:13:26 +0530

    Anagram
    Optimized anagram

---
+++ 2D Array/2DArrayFrom1DArray.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<vector>
+using namespace std; 
+
+vector<vector<int>> 1dTo2dArray(vector<int>& original, int m, int n){
+
+}
+
+int main (){
+    vector<vector<int>> arr; 
+    vector<int> original = {1, 2, 3, 4, 5, 6};
+}
