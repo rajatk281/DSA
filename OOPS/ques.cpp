c53b3aff3229acca9aa56380fbfd4388ee9984ae
NNobittaa <rajat28012006@gmail.com>
2026-04-11 11:24:08 +0530

    Parameterized constructor

---
+++ OOPS/ques.cpp
@@ -0,0 +1,28 @@
+//  Create a user class with the properties : id(private), username(public) & password(private). It should be initialized in a parameterized constructor. It should a getter and setter for password
+
+#include<iostream>
+#include<string>
+using namespace std ; 
+
+class Person {
+    int id ; 
+    int password;
+    public:
+        string username;
+        Person (string name, int newPassword){
+            username = name ;
+            password = newPassword; 
+        }
+        void setPassword (int newPass){
+            password = newPass;
+        }
+        int getPassword(){
+            return password;
+        }
+};
+
+int main (){
+    Person P1("Rajat", 12345);
+    cout<<P1.getPassword()<<endl;
+    cout<<P1.username;
+}
