395b8b34b5979ed310cfdf7c31f2818d76d5fedf
Rajat Kumar <rajat28012006@gmail.com>
2026-06-15 16:40:28 +0530

    Small fix

--- Striver/Patterns.cpp
+++ Striver/Patterns.cpp
@@ -288,7 +288,7 @@ void eigtheenthPattern(int N){
 void nineteenthPattern(int N){
     for (int i=1; i<=2*N; i++){
         for (int j=1; j<=2*N; j++){
-            if(i<=5){
+            if(i<=N){
                 if(j<=N-(i-1) || j>N+(i-1) ){
                     cout<< "*";
                 } else {
@@ -308,5 +308,5 @@ void nineteenthPattern(int N){
 
 int main()
 {
-    nineteenthPattern(5);
+    nineteenthPattern(10);
 }
