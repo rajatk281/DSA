4a0d6a4a53418da8da2e6a703987bbca4ca6cffd
Rajat Kumar <rajat28012006@gmail.com>
2026-06-28 11:33:27 +0530

    Insertion sort

--- Striver/Sorting/sortingTech.cpp
+++ Striver/Sorting/sortingTech.cpp
@@ -33,6 +33,21 @@ void selectionSort(int arr[], int size){
     }
 }
 
+
+
+void insertionSort(int arr[], int size){
+    for (int i=1; i<size; i++){
+        int curr = arr[i];
+        int prev = i-1; 
+        while (prev>=0 && arr[prev]>curr){
+            arr[prev+1] = arr[prev];
+            prev--;
+        }
+        swap(arr[prev+1], curr);
+    }
+}
+
+
 void print(int arr[], int size){
     for (int i=0; i<size; i++){
         cout << arr[i] << " ";
@@ -41,6 +56,6 @@ void print(int arr[], int size){
 
 int main (){
     int arr[] = {5, 3,2 ,1, 4};
-    selectionSort(arr, sizeof(arr)/sizeof(int));
+    insertionSort(arr, sizeof(arr)/sizeof(int));
     print(arr, sizeof(arr)/sizeof(int));
 }
