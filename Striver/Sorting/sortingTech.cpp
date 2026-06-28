baf4b61fe378f39f567bbb57820150132b2451dd
Rajat Kumar <rajat28012006@gmail.com>
2026-06-28 20:38:16 +0530

    Recursive Insetion sort

--- Striver/Sorting/sortingTech.cpp
+++ Striver/Sorting/sortingTech.cpp
@@ -11,12 +11,35 @@ void bubbleSort(int arr[], int size){
                 swap(arr[j], arr[j+1]);
                 isSwap=true; 
             }
-        }
+        }   
     }
     // Optimization 
     if (!isSwap){
         return; 
     }
+}
+
+void recBubbleSort(int arr[], int n){
+    // Base case
+    if (n == 1)
+        return;
+        
+    bool isSwap = false;
+
+    // One complete pass
+    for (int i = 0; i < n - 1; i++) {
+        if (arr[i] > arr[i + 1]) {
+            swap(arr[i], arr[i + 1]);
+            isSwap = true;
+        }
+    }
+
+    // Optimization
+    if (!isSwap)
+        return;
+
+    // Recursive call for remaining array
+    bubbleSort(arr, n - 1);
 }
 
 // We select sorted and unsorted part of an array and compare for the smallest idx element wrt to the ith element in each iteration .
@@ -33,7 +56,6 @@ void selectionSort(int arr[], int size){
     }
 }
 
-
 // Based on placing card games, we insert the each element in its correct position.
 // Time complexity : O(n^2)
 void insertionSort(int arr[], int size){
@@ -48,6 +70,18 @@ void insertionSort(int arr[], int size){
     }
 }
 
+void recInsertionSort(int arr[], int size, int i){
+    if (i>=size)  return ;
+    
+    int prev = i-1; 
+    int curr = arr[i];
+    while(prev>=0 && arr[prev]>curr){
+        arr[prev+1] = arr[prev];
+        prev--; 
+    }
+    arr[prev + 1] = curr;
+    recInsertionSort(arr, size, i+1);
+}
 
 void print(int arr[], int size){
     for (int i=0; i<size; i++){
@@ -57,6 +91,6 @@ void print(int arr[], int size){
 
 int main (){
     int arr[] = {5, 3,2 ,1, 4};
-    insertionSort(arr, sizeof(arr)/sizeof(int));
+    recInsertionSort(arr, sizeof(arr)/sizeof(int), 1);
     print(arr, sizeof(arr)/sizeof(int));
 }
