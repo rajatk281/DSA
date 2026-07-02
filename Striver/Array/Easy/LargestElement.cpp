3145ade97b5987dfd982093e5cdf74bb18f36a9d
Rajat Kumar <rajat28012006@gmail.com>
2026-07-02 11:16:35 +0530

    Optimised version

--- Striver/Array/Easy/LargestElement.cpp
+++ Striver/Array/Easy/LargestElement.cpp
@@ -8,7 +8,24 @@ int largestElement(vector<int> arr){
     return arr[arr.size()-1];
 }
 
+// Optimised Version 
+int optLargestElement(vector<int> arr){
+    int st=0; int end=arr.size()-1;
+    int grt = 0; 
+    while(st<=end){
+        if (arr[st]<=arr[end] && arr[end]>grt){
+            grt = arr[end];
+        }else if (arr[st]>arr[end] && arr[st]>grt){
+            grt = arr[st];
+        }
+        st++;
+        end--; 
+    }
+    return grt; 
+
+}
+
 int main (){
     vector<int> arr = {1, 7, 3, 12, 10, 6, 4};
-    cout<<largestElement(arr);
+    cout<<optLargestElement(arr);
 }
