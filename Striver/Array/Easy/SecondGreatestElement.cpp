59fcc3f51a4c63b061b111375ad05b1a29553d66
Rajat Kumar <rajat28012006@gmail.com>
2026-07-03 12:59:51 +0530

    Second largest and second smallest values in an array

--- Striver/Array/Easy/SecondGreatestElement.cpp
+++ Striver/Array/Easy/SecondGreatestElement.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<vector>
+#include<stdlib.h>
 #include<algorithm>
 using namespace std; 
 
@@ -10,12 +11,33 @@ pair<int, int> secGreatElement(vector<int> arr){
 }
 
 // Optimized approach 
-pair<int, int> optSecGreatElement(vector<int> arr){
-    int minn=arr[0];
-    
+void optSecGreatElement(vector<int> arr){
+    if (arr.size()==1 || arr.size()==0){
+        cout<<-1<<" "<<-1<<endl; 
+        return; 
+    }
+    int small=INT_MAX, secondSmall=INT_MAX;
+    int great = INT_MIN, secondGreat = INT_MIN; 
+    int st=0, end=arr.size()-1;
+    int i ; 
+    for (i=0; i<arr.size(); i++){
+        small = min(small, arr[i]);
+        great = max(great, arr[i]);
+    }
+    for (i=0; i<arr.size(); i++){
+        if (arr[i]<secondSmall && arr[i]!=small){
+            secondSmall = arr[i];
+        }
+        if (arr[i]>secondGreat && arr[i]!=great){
+            secondGreat = arr[i];
+        }
+    }
+    cout<<"Second Largest value: "<<secondGreat<<endl; 
+    cout<<"Second Smallest value: "<<secondSmall<<endl; 
+    return ;
 }
 
 int main (){
-    vector<int> arr = {1, 7, 3, 12, 10, 6, 4};
-    cout<<secGreatElement(arr).second<<secGreatElement(arr).first<<", ";
+    vector<int> arr = {5, 1, 9, 2, 8};
+    optSecGreatElement(arr);   
 }
