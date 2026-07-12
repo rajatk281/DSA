4aa789d076cd2cf03f9d437e44c0428b8ad3114c
Rajat Kumar <rajat28012006@gmail.com>
2026-07-12 12:14:06 +0530

    Optimised approach for missing num

--- Striver/Array/Easy/MissingNumber.cpp
+++ Striver/Array/Easy/MissingNumber.cpp
@@ -16,7 +16,21 @@ int missingNum(vector<int> arr){
     return -1; 
 
 }
+
+int OptMissingNum(vector<int> arr){
+    int giveNum = 0; 
+    long long expectedSum = 0; 
+    for (int i=0; i<arr.size(); i++){
+        giveNum+=arr[i];
+    }
+    for (int i=1; i<=arr.size()+1; i++){
+        expectedSum+=i; 
+    } 
+    int missingNum = expectedSum-giveNum; 
+    return missingNum; 
+}
+
 int main (){
-    vector<int> arr={1, 2, 3, 5};
-    cout<< missingNum(arr);
+    vector<int> arr={8, 2, 4, 5, 3, 7, 1};
+    cout<< OptMissingNum(arr);
 }
