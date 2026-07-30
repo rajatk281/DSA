4f8979af7d66bc5e338fb78093c9c4784ff459e1
Rajat <rajat28012006@gmail.com>
2026-07-30 09:12:06 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (71.21%) - LeetSync

--- Leetcode/50-powx-n/powx-n.cpp
+++ Leetcode/50-powx-n/powx-n.cpp
@@ -1,24 +1,20 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-        long long N = n;
-
-        if (N < 0) {
-            x = 1 / x;
-            N = -N;
+        long binForm = n; 
+        if (n==0) return 1; 
+        if (n<0){
+            x = 1/x; 
+            binForm = -binForm; 
         }
-
-        return power(x, N);
-    }
-
-    double power(double x, long long n) {
-        if (n == 0) return 1;
-
-        double half = power(x, n / 2);
-
-        if (n % 2 == 0)
-            return half * half;
-        else
-            return x * half * half;
+        double ans = 1; 
+        while(binForm>0){
+            if (binForm % 2 == 1){
+                ans*= x; 
+            }
+            x *= x;
+            binForm /= 2; 
+        }
+        return ans; 
     }
 };
