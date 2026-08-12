a51c02c873845cf9e6fc81f26b2f3c3e69c2ff58
Rajat <rajat28012006@gmail.com>
2026-08-12 09:03:27 +0530

    Time: 3 ms (27.75%) | Memory: 97.4 MB (25.93%) - LeetSync

--- Leetcode/121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
+++ Leetcode/121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -4,7 +4,7 @@ public:
         int profit = 0;
         int buyPrice = nums[0];
         for (int i=1; i<nums.size(); i++){
-            buyPrice = min(buyPrice, nums[i]);
+            buyPrice = min(buyPrice, nums[i-1]);
             profit = max(profit, nums[i] - buyPrice);
         }
         return profit; 
