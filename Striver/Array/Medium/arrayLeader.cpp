f7a563dc89cc54715792c1c5c0f3cc7ae9d4f719
Rajat Kumar <rajat28012006@gmail.com>
2026-08-14 09:22:41 +0530

    Optimization

--- Striver/Array/Medium/arrayLeader.cpp
+++ Striver/Array/Medium/arrayLeader.cpp
@@ -9,9 +9,9 @@ vector<int> arrLeaders(vector<int> &arr){
     result.push_back(maxEl);
     i--;
     while(i>=0){
-        maxEl = max(maxEl, arr[i+1]);
         if (arr[i]>maxEl){
             result.push_back(arr[i]);
+            maxEl = arr[i];
         }
         i--;
     }
