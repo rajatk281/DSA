92466586923516db4672644833ad9fe535396444
Rajat Kumar <rajat28012006@gmail.com>
2026-08-30 13:40:57 +0530

    Most Optimal Approach

--- Searching/BinarySearch/LowerBound.cpp
+++ Searching/BinarySearch/LowerBound.cpp
@@ -2,7 +2,8 @@
 #include<vector>
 using namespace std; 
 
-int lowerBound(vector<int> nums, int k){
+// Brute Force approach ( Linear Search )
+int L_lowerBound(vector<int> nums, int k){
     for (int i=0; i<nums.size(); i++){
         if (nums[i]>=k){
             return i; 
@@ -11,7 +12,27 @@ int lowerBound(vector<int> nums, int k){
     return -1; 
 }
 
+// Optimal Approach (Binary Search)
+int B_lowerBound(vector<int> nums, int k){
+    int low =0 ;
+    int high=nums.size()-1; 
+    int ans=nums.size(); 
+    while(low<=high){
+        int mid = low+(high-low)/2; 
+        if (nums[mid] >= k ) {
+           ans = mid; 
+           high = mid-1; 
+        }
+        else{
+            low=mid+1; 
+        }
+    }
+    return ans; 
+}
+
+
 int main (){
     vector<int> arr = {3,5,8,15,19};
-    cout<<lowerBound(arr, 9);
+    // cout<<L_lowerBound(arr, 9);
+    cout<<B_lowerBound(arr, 9);
 }
