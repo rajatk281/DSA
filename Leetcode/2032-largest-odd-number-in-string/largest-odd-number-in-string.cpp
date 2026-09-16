14d5b4e4117b5fb59263d7e792f34f07b25178b7
Rajat <rajat28012006@gmail.com>
2026-09-16 20:08:35 +0530

    Time: 106 ms (6.03%) | Memory: 17.8 MB (38.01%) - LeetSync

--- Leetcode/2032-largest-odd-number-in-string/largest-odd-number-in-string.cpp
+++ Leetcode/2032-largest-odd-number-in-string/largest-odd-number-in-string.cpp
@@ -2,14 +2,25 @@ class Solution {
 public:
     string largestOddNumber(string num) {
 
+        // Traverse the string from right to left
+        // because we need to find the rightmost odd digit
         for(int i = num.length() - 1; i >= 0; i--) {
 
+            // Convert character digit into integer using (num[i] - '0')
+            // Check if the current digit is odd
             if((num[i] - '0') % 2 != 0) {
+
+                // If an odd digit is found, return the substring
+                // from the beginning up to this odd digit
                 return num.substr(0, i + 1);
             }
-            cout<<num[i];
+
+            // Prints the current even digit (not needed for the solution)
+            cout << num[i];
         }
 
+        // If there is no odd digit in the entire string,
+        // then no odd number can be formed
         return "";
     }
 };
